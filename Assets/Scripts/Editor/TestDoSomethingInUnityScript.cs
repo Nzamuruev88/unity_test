fe1d55c0c39afd24584480e06d03c15a94e676f0
Nikolay Olegovic Zamuruev <nzamuruev1@goodgamestudios.com>
2022-12-22 09:53:41 +0100

    Updated test validation script

---
+++ Assets/Scripts/Editor/TestDoSomethingInUnityScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGS.Story.Build.Validation {
+    public class TestDoSomethingInUnityScript  {
+        public static void ValidateCommandLine() {
+            Debug.Log("Test");
+        }
+    }
+}
